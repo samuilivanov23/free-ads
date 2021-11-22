c9e4978ab159f1d47ff6e8e82e212441d5166198
samuilivanov23 <samuil2001ivanov@gmail.com>
2021-11-23 00:06:55 +0200

    (23-11-2021) Initial commit. Dumping maven spring boot project with postgresql database connection test.

---
+++ freeads/src/main/java/com/freeads/freeads/Greeting.java
@@ -0,0 +1,16 @@
+package com.freeads.freeads;
+
+public class Greeting
+{
+	private Long id;
+	private String content;
+
+	public Greeting( Long id, String content )
+	{
+		this.id = id;
+		this.content = content;
+	}
+
+	public Long getId() { return this.id; }
+	public String getContent() { return this.content; }
+}
