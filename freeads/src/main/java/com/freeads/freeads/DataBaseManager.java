02d04ef2a2f46426e229ff090118eefd21a96f10
samuilivanov23 <samuil2001ivanov@gmail.com>
2021-11-25 01:02:24 +0200

    (25-11-2021) Adding custom tomcat configuration. Adding exception handling while reading the DB config file

--- freeads/src/main/java/com/freeads/freeads/DataBaseManager.java
+++ freeads/src/main/java/com/freeads/freeads/DataBaseManager.java
@@ -1,6 +1,7 @@
 package com.freeads.freeads;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.DatabaseMetaData;
@@ -12,52 +13,42 @@ public class DataBaseManager
 	{
 		try
 		{
-			// Create Properties object.
 			Properties props = new Properties();
-
-			String dbSettingsPropertyFile = "/home/samuil/JDBCSettings.properties";
-			// Properties will use a FileReader object as input.
+			String dbSettingsPropertyFile = "/etc/freeadsapp/JDBCSettings.properties";
+					
 			FileReader fReader = new FileReader(dbSettingsPropertyFile);
-
-			// Load jdbc related properties in above file.
 			props.load(fReader);
 
-			// Get each property value.
 			String dbDriverClass = props.getProperty("db.driver.class");
-
 			String dbConnUrl = props.getProperty("db.conn.url");
-
 			String dbUserName = props.getProperty("db.username");
-
 			String dbPassword = props.getProperty("db.password");
-			System.out.println( dbPassword );
-			System.out.println( dbConnUrl );
 
-			if(!"".equals(dbDriverClass) && !"".equals(dbConnUrl))
+			try
 			{
-				/* Register jdbc driver class. */
-				Class.forName(dbDriverClass);
+				if(!"".equals(dbDriverClass) && !"".equals(dbConnUrl))
+				{
+					/* Register jdbc driver class. */
+					Class.forName(dbDriverClass);
 
-				// Get database connection object.
-				Connection dbConn = DriverManager.getConnection(dbConnUrl, dbUserName, dbPassword);
+					Connection dbConn = DriverManager.getConnection(dbConnUrl, dbUserName, dbPassword);
+					DatabaseMetaData dbMetaData = dbConn.getMetaData();
+					String dbName = dbMetaData.getDatabaseProductName();
+					String dbVersion = dbMetaData.getDatabaseProductVersion();
 
-				// Get dtabase meta data.
-				DatabaseMetaData dbMetaData = dbConn.getMetaData();
-
-				// Get database name.
-				String dbName = dbMetaData.getDatabaseProductName();
-
-				// Get database version.
-				String dbVersion = dbMetaData.getDatabaseProductVersion();
-
-				System.out.println("Database Name : " + dbName);
-
-				System.out.println("Database Version : " + dbVersion);
+					System.out.println("Database Name : " + dbName);
+					System.out.println("Database Version : " + dbVersion);
+				}
+			}
+			catch(Exception exception)
+			{
+				exception.printStackTrace();
 			}
 		}
-		catch(Exception ex)
+		catch ( IOException exception )
 		{
-			ex.printStackTrace();
+			System.out.println( "Db configuration file not found at /etc/freeadsapp/JDBCSettings.properties!" );
+			exception.printStackTrace();
 		}
 	}
 }
